71825e7202b5fb4c142a854bc648c9b6d19d0e0b
alexdenker <adenker@uni-bremen.de>
2025-10-22 09:57:50 +0100

    change NFFT backbone

--- NonUniformMesh/neural_operator/nfft_neural_operator.py
+++ NonUniformMesh/neural_operator/nfft_neural_operator.py
@@ -27,28 +27,76 @@ def timestep_embedding(timesteps, dim, max_period=10000):
     return embedding
 
 
+
+# class for fully nonequispaced 2d points
+class VFT:
+    def __init__(self, x_positions, y_positions, modes):
+        # it is important that positions are scaled between 0 and 2*pi
+        x_positions -= torch.min(x_positions)
+        self.x_positions = x_positions * 6.28 / (torch.max(x_positions))
+        y_positions -= torch.min(y_positions)
+        self.y_positions = y_positions * 6.28 / (torch.max(y_positions))
+        self.number_points = x_positions.shape[1]
+        self.batch_size = x_positions.shape[0]
+        self.modes = modes
+
+        self.X_ = torch.cat((torch.arange(modes), torch.arange(start=-(modes), end=0)), 0).repeat(self.batch_size, 1)[:,:,None].float().cuda()
+        self.Y_ = torch.cat((torch.arange(modes), torch.arange(start=-(modes-1), end=0)), 0).repeat(self.batch_size, 1)[:,:,None].float().cuda()
+
+
+        self.V_fwd, self.V_inv = self.make_matrix()
+
+    def make_matrix(self):
+        m = (self.modes*2)*(self.modes*2-1)
+        X_mat = torch.bmm(self.X_, self.x_positions[:,None,:]).repeat(1, (self.modes*2-1), 1)
+        Y_mat = (torch.bmm(self.Y_, self.y_positions[:,None,:]).repeat(1, 1, self.modes*2).reshape(self.batch_size,m,self.number_points))
+        forward_mat = torch.exp(-1j* (X_mat+Y_mat)) 
+
+        inverse_mat = torch.conj(forward_mat.clone()).permute(0,2,1)
+
+        return forward_mat, inverse_mat
+
+    def forward(self, data):
+        """
+        data: (batch_size, num_mesh_points, num_features) 
+        """
+
+        data_fwd = torch.bmm(self.V_fwd, data)
+        return data_fwd
+
+    def inverse(self, data):
+        """
+        data: (batch_size, num_modes, num_features) 
+        """
+
+        data_inv = torch.bmm(self.V_inv, data)
+        
+        return data_inv
+    
+
 class SpectralConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, modes1, modes2, emb_dim):
+    def __init__(self, in_channels, out_channels, modes, emb_dim):
         super(SpectralConv2d, self).__init__()
 
         """
         2D Fourier layer. It does NFFT, linear transform, and Adjoint NFFT.    
         """
 
-        self.nfft = NFFT((modes1, modes2))
-
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.modes1 = modes1 
-        self.modes2 = modes2
+        self.modes = modes
 
         self.scale = (1 / (in_channels * out_channels))
-        self.weights = nn.Parameter(
-            self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights1 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes, self.modes, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, self.modes, self.modes, dtype=torch.cfloat))
         
         # time modulation scaling 
-        self.emd_modulation = nn.Parameter(
-            self.scale * torch.rand(1, self.modes1, self.modes2, emb_dim, dtype=torch.cfloat))
+        self.emd_modulation1 = nn.Parameter(
+            self.scale * torch.rand(1, self.modes, self.modes, emb_dim, dtype=torch.cfloat))
+        self.emd_modulation2 = nn.Parameter(
+                    self.scale * torch.rand(1, self.modes, self.modes, emb_dim, dtype=torch.cfloat))
 
 
     # Complex multiplication
@@ -56,29 +104,61 @@ class SpectralConv2d(nn.Module):
         # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
         return torch.einsum("bixy,ioxy->boxy", input, weights)
 
-    def forward(self, x, pos, emb):
+    def forward(self, x, nuft, emb):
         """
         Inputs:
         x (torch.Tensor): features with shape [batch_size, in_channels, num_points]
-        pos (torch.Tensor): grid points [batch_size, 1, num_points, 2] (have to be scaled in [-0.5, 0.5] for the nfft to work) 
+        nuft: class VFT
         emb (torch.Tensor): time embedding vector [batch_size, emb_dim]
 
         Output:
         transformed features, shape [batch_size, out_channels, num_points]
         """
+        batchsize = x.shape[0]
 
-        x_nfft = self.nfft.adjoint(pos, x)
-        print("x_nfft: ", x_nfft.shape)
+        x_nfft = nuft.forward(x.permute(0, 2,1).cfloat())
+        x_ft = x_nfft.permute(0, 2, 1)
+        x_ft = torch.reshape(x_ft, (x.shape[0], self.out_channels, 2*self.modes, 2*self.modes-1))
+
+
         emb = emb[:,None,None,:] + 0j 
-        phi = torch.einsum('bmwc,bmwc->bmw', self.emd_modulation, emb)
+        phi1 = torch.einsum('bmwc,bmwc->bmw', self.emd_modulation1, emb)
+        phi2 = torch.einsum('bmwc,bmwc->bmw', self.emd_modulation2, emb)
 
-        x_nfft = self.compl_mul2d(x_nfft, self.weights)
-        x_nfft = torch.einsum('bij,bfij->bfij', phi, x_nfft)
+        # Spectral convolution
+        out_ft = torch.zeros(batchsize, self.out_channels, 2*self.modes, self.modes, dtype=torch.cfloat, device=x.device)
+        out_ft1 = self.compl_mul2d(x_ft[:, :, :self.modes, :self.modes], self.weights1)
+        out_ft2 = self.compl_mul2d(x_ft[:, :, -self.modes:, :self.modes], self.weights2)
+        out_ft[:, :, :self.modes, :self.modes] = out_ft1
+        out_ft[:, :, -self.modes:, :self.modes] = out_ft2
 
-        x_ifft = self.nfft(pos, x_nfft) 
-        x_ifft = x_ifft / x_ifft.size(-1) * 2
+        # Modulation
+        modified1 = torch.einsum('bij,bfij->bfij', phi1, out_ft[:, :, :self.modes, :self.modes])
+        modified2 = torch.einsum('bij,bfij->bfij', phi2, out_ft[:, :, -self.modes:, :self.modes])
+
+        # Build output tensor safely
+        out_ft_mod = out_ft.clone()
+        out_ft_mod[:, :, :self.modes, :self.modes] = modified1
+        out_ft_mod[:, :, -self.modes:, :self.modes] = modified2
+
+
+        x_ft = torch.reshape(out_ft_mod, (batchsize, self.out_channels, 2*self.modes**2))
+        x_ft2 = x_ft[..., 2*self.modes:].flip(-1, -2).conj()
+        x_ft = torch.cat([x_ft, x_ft2], dim=-1)
+
+        x_ft = x_ft.permute(0, 2, 1)
+        x = nuft.inverse(x_ft) 
+        x = x.permute(0, 2, 1)
+        x = x / x.size(-1) * 2
+
+
+        #x_nfft = self.compl_mul2d(x_nfft, self.weights)
+        #x_nfft = torch.einsum('bij,bfij->bfij', phi, x_nfft)
+
+        #x_ifft = self.nfft(pos, x_nfft) 
+        #x_ifft = x_ifft / x_ifft.size(-1) * 2
         
-        return x_ifft.real
+        return x.real
 
 
 
@@ -116,7 +196,7 @@ class NUFNOBlock(nn.Module):
         self.width = width
         self.emb_dim = emb_dim
 
-        self.spectral_conv = SpectralConv2d(self.width, self.width, self.modes1, self.modes2, self.emb_dim)
+        self.spectral_conv = SpectralConv2d(in_channels=self.width, out_channels=self.width, modes=self.modes1, emb_dim=self.emb_dim)
         self.conv1d = Conv1DTime(self.width, self.emb_dim)
 
         self.act = nn.SiLU()
@@ -142,6 +222,7 @@ class NUFNO(nn.Module):
         super(NUFNO, self).__init__()
         self.timestep_embedding_dim = timestep_embedding_dim
         self.max_period = max_period
+        self.modes = modes 
 
         self.fc0 = nn.Linear(in_channels, width)
 
@@ -160,13 +241,15 @@ class NUFNO(nn.Module):
         t: [batch_size,] 
         pos: [batch_size, 1, num_points, 2] grid points
         """
+        nuft = VFT(pos[:,0,:,0], pos[:,0,:,1], modes=self.modes)
+
 
         emb = timestep_embedding(t, self.timestep_embedding_dim, max_period=self.max_period)
         x = x.permute(0, 2, 1)
         x = self.fc0(x)
         x = x.permute(0, 2, 1).contiguous()
         for layer in self.layers:
-            x = layer(x, pos, emb)
+            x = layer(x, nuft, emb)
 
         x = x.permute(0, 2, 1)
         x = self.fc1(x)
@@ -180,8 +263,6 @@ if __name__ == "__main__":
     
     batch_size = 8
     num_points = 1600
-
-
 
 
     model = NUFNO(n_layers=4, modes=12, width=32, timestep_embedding_dim=33)
