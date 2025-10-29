11846ed902f9a4f68e7f68707e52c056001bb0c5
alexdenker <adenker@uni-bremen.de>
2025-10-29 16:55:03 +0000

    add exponential integrator

--- NonUniformMesh/samplers/euler_maruyama.py
+++ NonUniformMesh/samplers/euler_maruyama.py
@@ -31,6 +31,45 @@ class EulerMaruyama():
             beta_t = self.sde.beta_t(t).view(-1, 1, 1)
             noise = self.noise_sampler.sample(batch_size).unsqueeze(1) # N(0,C)
             
-            xt = xt + beta_t/2.0 * delta_t*xt + beta_t* delta_t * score + beta_t.sqrt()*delta_t.sqrt() * noise 
+            xt = xt + delta_t * beta_t/2.0 * xt + delta_t * beta_t * score + delta_t.sqrt() * beta_t.sqrt() * noise 
+
+        return xt 
+    
+
+class EulerMaruyamaExponential():
+    """
+    Make use of an exponential integrator 
+    
+    """
+    def __init__(self, model, cfg: DictConfig):
+        self.model = model 
+        self.sde = model.sde
+        self.noise_sampler = model.noise_sampler 
+        
+        self.cfg = cfg 
+
+    def sample(self, pos, ts, **kwargs):
+        """
+        pos: positions, [B, N, D]        
+        ts: timesteps, [T] (forward in time)
+        """
+
+        batch_size = pos.shape[0]
+
+        delta_t = ts[1] - ts[0]
+        xt = self.noise_sampler.sample(batch_size).unsqueeze(1) # N(0,C)
+
+        for ti in tqdm(reversed(ts), total=len(ts)):
+            t = torch.ones(batch_size).to(xt.device)* ti
+
+            with torch.no_grad():
+                score, _ = self.model(xt, t, pos)
+
+            beta_t = self.sde.beta_t(t).view(-1, 1, 1)
+            noise = self.noise_sampler.sample(batch_size).unsqueeze(1) # N(0,C)
+            
+            phi_t = torch.exp(1/2*beta_t*delta_t)
+            xt = phi_t*xt + 2*(phi_t - 1) * score + (phi_t**2 - 1).sqrt() * noise 
+            #xt = xt + delta_t * beta_t/2.0 * xt + delta_t * beta_t * score + delta_t.sqrt() * beta_t.sqrt() * noise 
 
         return xt 
